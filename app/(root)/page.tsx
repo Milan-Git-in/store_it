17feff81c4daba6e8ebca06fe7668ff27ebc461d
Milan Prajapati <milanprajapati366@gmail.com>
2024-11-15 14:22:19 +0530

    Finish Store It

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="table-header-group">
+        Store it The only Solution you need
+      </h1>
+    </div>
+  );
+}
